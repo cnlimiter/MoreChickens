6124403803cc54b560f6ea7cffbc534d311d1317
cnlimiter <cnlimiter@163.com>
2022-06-11 13:34:59 +0800

    rebuild BaseChickenEntity

---
+++ src/main/java/cn/evolvefield/mods/morechickens/common/entity/ModAnimalEntity.java
@@ -0,0 +1,30 @@
+package cn.evolvefield.mods.morechickens.common.entity;
+
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.Level;
+
+public abstract class ModAnimalEntity extends Animal {
+
+    protected ModAnimalEntity(EntityType<? extends Animal> type, Level worldIn) {
+        super(type, worldIn);
+    }
+
+    protected abstract int getBreedingTimeout();
+
+    @Override
+    public void aiStep() {
+        super.aiStep();
+        int timeout = getBreedingTimeout();
+        if(getAge() > timeout)
+            setAge(timeout);
+        if(timeout > 6000){
+            if(getAge() == 5999)
+                setAge(timeout);
+            else if(getAge() == 6000)
+                setAge(5999);
+        }
+    }
+
+}
